006a38b9cb2dd8a8477b87178814efb7ed7b69f9
Albert Mabunda <mabundaalbert7@gmail.com>
2026-06-01 21:55:38 +0100

    Add Google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import './globals.css'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: 'MailConnector',
+  description: 'Meet people through meaningful letters',
+  verification: {
+    google: 'Qa1zLxLHdEq5a7xtUdS0rUOwxN7N0hnmCt2IkBHFdA8'
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
